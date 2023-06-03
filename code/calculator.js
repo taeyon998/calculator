1fc0c091a8c5f8e05c646f2cc156df195db26ff0
Taeyon Kim <taeyon998@gmail.com>
2023-06-03 04:53:01 +0000

    a

--- code/calculator.js
+++ code/calculator.js
@@ -1,1 +1,23 @@
-function add()
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+let n1, op, n2;
+
+function operate(a, op, b) {
+    switch (op) {
+        case '+':
+            return add(a,b);
+            break;
+        case '-':
+            return substract(a,b);
+            break;
+        case '*':
+            return multiply(a,b);
+            break;
+        case '/':
+            return divide(a,b);
+            break;
+    }
+}
