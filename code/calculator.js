409f825927d90fa06d7c7450b272ea4dc20033fa
Taeyon Kim <taeyon998@gmail.com>
2023-06-18 11:00:09 +0000

    d

--- code/calculator.js
+++ code/calculator.js
@@ -1,4 +1,4 @@
-const screenSection = document.querySelector('#screenSection'); 
+const screenSection = document.querySelector('#screenSection');
 
 let displayValue = '0';
 let firstOperand = null; // "3" + 5
@@ -19,16 +19,16 @@ let n1, op, n2;
 function operate(a, op, b) {
     switch (op) {
         case '+':
-            return add(a,b);
+            return add(a, b);
             break;
         case '-':
-            return substract(a,b);
+            return subtract(a, b);
             break;
         case '*':
-            return multiply(a,b);
+            return multiply(a, b);
             break;
         case '/':
-            return divide(a,b);
+            return divide(a, b);
             break;
     }
 }
@@ -44,19 +44,20 @@ function initialize() {
     buttons.forEach((button) => {
         button.addEventListener('click', display);
     });
+
+    display();
 }
 
+// EFFECT: show displayValue
 function display(event) {
 
-    while (screenSection.firstChild){
+    while (screenSection.firstChild) {
         screenSection.firstChild.remove(); // clear screen
     }
 
-    const button = event.target;
-
     const num = document.createElement('div');
-    
-    num.textContent = `${button.textContent}`;
+
+    num.textContent = displayValue;
     num.style.textAlign = 'right';
     num.style.width = `600px`;
     screenSection.append(num);
@@ -65,62 +66,98 @@ function display(event) {
 initialize();
 
 // EFFECT: user clicks button '5' -> inputOperand(5) -> updates display from '0' -> '5'
-function inputOperand(v){
+function inputOperand(v) {
     // "55" + 32 -> Don't update firstOperand, until user inputs operator!
-    if(firstOperand === null){
-        if(displayValue==='0'){ // Eg. By default, calculator displays '0'. We don't want it to become '05', we want it to be '5'
+    if (firstOperand === null) {
+        if (displayValue === '0') { // Eg. By default, calculator displays '0'. We don't want it to become '05', we want it to be '5'
             displayValue = v;
         }
-        else{
+        else {
             displayValue += v; // Eg. '7' -> '75'
         }
     }
     // 55 + "32" -> After user inputs operator
     else {
-        if (displayValue===firstOperand){ // 3rd click, when inputting "3". Basically, display is still at 55, which is firstOperand. Update display from "55" -> "3"
+        if (displayValue === firstOperand) { // 3rd click, when inputting "3". Basically, display is still at 55, which is firstOperand. Update display from "55" -> "3"
             displayValue = v;
         }
         else { // When inputting "2". Update display from "3" -> "32"
             displayValue += v;
-        }        
+        }
     }
 }
 
 // EFFECT: user clicks '+' -> inputOperator('+') -> updates firstOperand from null -> '55'
-function inputOperator(op){
+function inputOperator(op) {
     // Case 1: firstOperand = null, firstOperator = null -> (1) update firstOperator = '+', (2) update firstOperand = '55'
-    if (firstOperand===null && firstOperator===null) {
+    // Eg. User inputs "55" then "+"
+    if (firstOperand === null && firstOperator === null) {
         firstOperator = op;
         firstOperand = displayValue;
     }
-    // Case 2: firstOperand = '55', firstOperator = '+', SecondOperand = null -> (1) update firstOperator = '+'
-    // Eg. User inputs "55 +", then changes operator to "*" 
-    else if (firstOperand!=null && firstOperator!=null && secondOperand === null){
+    // Case 2:
+    // Eg. User inputs "5 + 3 =", then inputs "+"
+    else if (firstOperand != null && firstOperator === null) {
         firstOperator = op;
     }
-    // Case 3: firstOperand = '55', firstOperator = '+', SecondOperand = '32' -> (1) update result from null -> '87', (2) update display from '55' -> '87', (3) update firstOperator = '+', (4) update SecondOperand = null
-    // Eg. User inputs "55 + 33", then inputs "+" again
-    else if (firstOperand!=null && firstOperator!=null && secondOperand != null){
-        result = operate(firstOperand, firstOperator, secondOperand);
+    // Case 3: firstOperand = '55', firstOperator = '+' -> (0) update secondOperant from null to '32' (1) update result from null -> '87', (2) update display from '55' -> '87', (3) update firstOperator = '+', (4) update SecondOperand = null
+    // Eg. User inputs "55 + 32", then inputs "+" again    
+    else{
+        secondOperand = displayValue;
+        result = operate(Number(firstOperand), firstOperator, Number(secondOperand));
         displayValue = result;
         firstOperator = op;
+        firstOperand = result;
         secondOperand = null;
-    }
-    else {
-        alert("error on inputerOperator(op)!")
     }
 }
 
-// 
+function clearDisplay() {
+    displayValue = '0';
+    firstOperand = null; // "3" + 5
+    secondOperand = null; // 3 + "5"
+    firstOperator = null; // 3 "+" 5
+    result = null;
+}
+
+function inputEquals(){
+    secondOperand = displayValue;
+    result = operate(Number(firstOperand), firstOperator, Number(secondOperand));
+    displayValue = result;
+    firstOperand = result; // "3" + 5
+    secondOperand = null; // 3 + "5"
+    firstOperator = null; // 3 "+" 5
+    result = null;
+}
 
 // add EVENT LISTENERS to all buttons
 function clickButton() {
-    for(i=0; i<buttons.length; i++){
-        buttons[i].addEventListener('click',function() {
-            if (buttons[i].classList.contains('operand')){
-                inputOperand(buttons[i].value);
+    console.log(`${buttons.length}`);
+    for (let i = 0; i < buttons.length; i++) {
+
+        buttons[i].addEventListener('click', function () {
+            if (buttons[i].classList.contains('operand')) {
+                console.log(`${buttons[i]}`);
+                inputOperand(buttons[i].textContent);
                 display();
+            }
+            else if (buttons[i].classList.contains('operator')) {
+                inputOperator(buttons[i].textContent);
+                display();
+            }
+            else if (buttons[i].classList.contains('clear')) {
+                clearDisplay();
+                display();
+            }
+            else if (buttons[i].classList.contains('equals')){
+                inputEquals();
+                display();
+            }
+            else {
+                alert(buttons[i].textContent);
             }
         })
     }
 }
+
+clickButton();
